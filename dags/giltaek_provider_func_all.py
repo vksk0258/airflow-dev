6672f2ea5840d6841fd61f9af2dfaac270225052
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2024-07-15 15:37:29 +0900

    모든 데이터 로드

---
+++ dags/giltaek_provider_func_all.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import pendulum
+import common.functions as fn
+
+with DAG(
+    dag_id="giltaek_provider_func_all",
+    # 이덱은 매일 6시 30분에 시작
+    schedule="30 6 * * *",
+    # 덱이 언제 부터 돌지 TZ=타임존
+    start_date=pendulum.datetime(2021, 1, 1, tz="Asia/Seoul"),
+    # 캐치업 배치 중간에 누락된 구간을 돌릴지 말지
+    # 1월 1일부터 3월 1일까지 누락된 덱을 한번에 돌아가게 된다 3월 1일에 왠만하면 false
+    catchup=False,
+    tags=["oracle", "snowflake", "pandas"]
+) as dag:
+    extract_from_oracle_all = PythonOperator(
+        task_id='extract_from_oracle_all',
+        python_callable=fn.extract_from_oracle_all
+    )
+
+    load_data_all = PythonOperator(
+        task_id='load_data_all',
+        python_callable=fn.load_data_all
+    )
+
+    extract_from_oracle_all >> load_data_all
--- plugins/common/functions.py
+++ plugins/common/functions.py
@@ -3,6 +3,46 @@ from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 import pandas as pd
 import pprint
 
+def extract_from_oracle_all():
+    oracle_hook = OracleHook(oracle_conn_id='ora_mason')
+    sql = """
+    SELECT * FROM (SELECT ENTITY_NAME, CITY, STATE_ABBREVIATION, VARIABLE_NAME, YEAR, MONTH, VALUE, UNIT, DEFINITION
+    FROM MASON.FINANCIAL_ENTITY_ANNUAL_TIME_SERIES)
+    """
+    connection = oracle_hook.get_conn()
+    cursor = connection.cursor()
+    data = cursor.fetchall()
+    column_names = [desc[0] for desc in cursor.description]
+    df = pd.DataFrame(data, columns=column_names)
+    cursor.close()
+    connection.close()
+    df_pivot = df.pivot_table(
+        index=['ENTITY_NAME', 'CITY', 'STATE_ABBREVIATION', 'YEAR'],
+        columns='VARIABLE_NAME',
+        values='VALUE',
+        aggfunc='first'
+    ).reset_index()
+    df_pivot.columns = [str(col) if isinstance(col, tuple) else col for col in df_pivot.columns]
+    df_transformed = df_pivot.rename_axis(None, axis=1).reset_index(drop=True)
+
+    pprint.pprint(df_transformed)
+    df_transformed.to_csv("/opt/airflow/plugins/files/bank_data_all.csv", header=False)
+
+def load_data_all():
+    snowflake_hook = SnowflakeHook(snowflake_conn_id='snow_itsmart')
+    connection = snowflake_hook.get_conn()
+    cursor = connection.cursor()
+    cursor.execute("PUT file:///opt/airflow/plugins/files/bank_data_all.csv @bank_stage")
+    # 데이터 로드
+    cursor.execute("""
+            COPY INTO FINANCIAL_SC
+            FROM @bank_stage/bank_data.csv
+            FILE_FORMAT = (TYPE = 'CSV', FIELD_OPTIONALLY_ENCLOSED_BY='"',  SKIP_HEADER = 1)
+        """)
+
+    connection.close()
+    cursor.close()
+
 def extract_from_oracle(**kwargs):
     oracle_hook = OracleHook(oracle_conn_id='ora_mason')
     sql = """
