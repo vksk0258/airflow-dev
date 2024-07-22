5fd1b46495135d5476c68a2eced8ff322a39c409
NB-4001957\DKSYSTEMS <moonoh.na@dongkuk.com>
2024-07-22 10:53:06 +0900

    코드 내용 수정

--- dags/moonoh_Snowflake_Transform_test02.py
+++ dags/moonoh_Snowflake_Transform_test02.py
@@ -17,7 +17,7 @@ default_args = {
 }
 
 dag = DAG(
-    'snowflake_transform_test',
+    'moonoh_snowflake_transform_test02',
     default_args=default_args,
     description='Extract from Oracle, transform, and load to Snowflake',
     schedule_interval='@daily',
